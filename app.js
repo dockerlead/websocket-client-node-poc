d0d1d4736ded125a0a8bb0998ea9d2fcf5f6a324
dockerlead <dockerlead@gmail.com>
2020-12-09 13:46:16 -0500

    :tada:Init and it just works

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const WebSocketClient = require('websocket').client;
+ 
+const client = new WebSocketClient();
+ 
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+ 
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if(message.type === 'utf8')
+            console.log(message.utf8Data);
+    });
+    
+});
+ 
+client.connect(process.env.WEBSOCKET_ENDPOINT);
